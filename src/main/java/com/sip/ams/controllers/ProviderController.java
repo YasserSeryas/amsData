b651f55975319372fb43f519bba8897b5627369c
moahmed amine mezghich <git@mezghich.com>
2023-11-26 10:17:54 +0100

    add de Provider

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sip.ams.entities.Provider;
 import com.sip.ams.services.ProviderService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/providers")
@@ -18,17 +24,7 @@ public class ProviderController {
 	@Autowired  //injection de dépendances
 	ProviderService service ;//= new ProviderService();
 	
-	@RequestMapping("/add")
-	@ResponseBody
-	public String add()
-	{
-		//Provider p1 = new Provider("Samsung","Koré SUD","samsung@hotmail.com");
-		//Provider res = service.addProvider(p1);
-		
-		Provider p2 = new Provider("HP","USA","hp@hotmail.com");
-		Provider res =service.addProvider(p2);
-		return res.toString();
-	}
+	
 	
 	@RequestMapping("/list")
 	@ResponseBody
@@ -36,5 +32,22 @@ public class ProviderController {
 		List<Provider> res = service.listProvider();
 		return res.toString();
 	}
+	
+	@GetMapping("add")
+    public String showAddProviderForm(Model model) {
+    	Provider provider = new Provider();// object dont la valeur des attributs par defaut
+    	model.addAttribute("provider", provider);
+        return "provider/addProvider";
+    }
+    
+    @PostMapping("add")
+    public String addProvider(@Valid Provider provider, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "provider/addProvider";
+        }
+        service.addProvider(provider);
+        return "redirect:list";
+    }
+
 
 }
