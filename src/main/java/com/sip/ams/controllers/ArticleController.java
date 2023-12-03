80284c989e1b36b3cd7a0f0a57e5235345581695
moahmed amine mezghich <git@mezghich.com>
2023-12-03 14:56:36 +0100

    update article + image + optiomisation du code dans le service Article

--- src/main/java/com/sip/ams/controllers/ArticleController.java
+++ src/main/java/com/sip/ams/controllers/ArticleController.java
@@ -1,14 +1,8 @@
 package com.sip.ams.controllers;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
+
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,178 +15,109 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sip.ams.entities.Article;
-import com.sip.ams.entities.Provider;
 import com.sip.ams.services.ArticleService;
 import com.sip.ams.services.ProviderService;
 
 import jakarta.validation.Valid;
 import org.springframework.web.multipart.MultipartFile;
+
 @Controller
 @RequestMapping("/articles/")
 
 public class ArticleController {
 
-	private  ArticleService articleService;
-	private  ProviderService providerService;
-	public static String uploadDirectory = System.getProperty("user.dir")+"/src/main/resources/static/uploadArticles";
-	
-    @Autowired
-    public ArticleController(ArticleService articleService, ProviderService providerService) {
-        this.articleService = articleService;
-        this.providerService = providerService;
-    }
-    
-    @GetMapping("list")
-    public String listProviders(Model model) {
- 
-    	List<Article>articles = articleService.listArticle();
-    	if (articles.size() == 0)
-    		articles = null;
-        model.addAttribute("articles", articles);
-        return "article/listArticles";
-    }
-    
-    @GetMapping("add")
-    public String showAddArticleForm(Model model) {
-    	
-    	model.addAttribute("providers", providerService.listProvider()); //récupération des providers
-    	model.addAttribute("article", new Article());
-        return "article/addArticle";
-    }
-    
-    @PostMapping("add")
-    //@ResponseBody
-    public String addArticle(Model model,@Valid Article article, BindingResult result, @RequestParam(name = "providerId", required = false) Long p,
-    		@RequestParam("files") MultipartFile[] files) {
-    	
-    	 	
-    	 if (result.hasErrors()) {
-    		 System.out.println("nb error : "+ result.getErrorCount());
-    		 // on doit récupérer de nouveaux la liste des providers en cas d'erreur
-    		 model.addAttribute("providers", providerService.listProvider());
-             return "article/addArticle";
-         }
-    	
-    	Provider provider = providerService.findProviderById(p)
-                .orElseThrow(()-> new IllegalArgumentException("Invalid provider Id:" + p));
-    	article.setProvider(provider);
-    	// upload de l'image
-    	
-    	MultipartFile file = files[0];
-    	long id = new Date().getTime();
-    	Path fileNameAndPath = Paths.get(uploadDirectory, ""+id+file.getOriginalFilename());
-    	
-    	StringBuilder fileName = new StringBuilder();
-    	
-    	fileName.append(""+id+file.getOriginalFilename()); //nom du ficher
-		  try {
-			Files.write(fileNameAndPath, file.getBytes()); //ecriture des bytes du fichier dans le nouveau emplacement
-		} catch (IOException e) {
-			e.printStackTrace();
+	private ArticleService articleService;
+	private ProviderService providerService;
+	public static String uploadDirectory = System.getProperty("user.dir") + "/src/main/resources/static/uploadArticles";
+
+	@Autowired
+	public ArticleController(ArticleService articleService, ProviderService providerService) {
+		this.articleService = articleService;
+		this.providerService = providerService;
+	}
+
+	@GetMapping("list")
+	public String listProviders(Model model) {
+
+		List<Article> articles = articleService.listArticle();
+		if (articles.size() == 0)
+			articles = null;
+		model.addAttribute("articles", articles);
+		return "article/listArticles";
+	}
+
+	@GetMapping("add")
+	public String showAddArticleForm(Model model) {
+
+		model.addAttribute("providers", providerService.listProvider()); // récupération des providers
+		model.addAttribute("article", new Article());
+		return "article/addArticle";
+	}
+
+	@PostMapping("add")
+	// @ResponseBody
+	public String addArticle(Model model, @Valid Article article, BindingResult result,
+			@RequestParam(name = "providerId", required = false) Long p, @RequestParam("files") MultipartFile[] files) {
+
+		if (result.hasErrors()) {
+			System.out.println("nb error : " + result.getErrorCount());
+			// on doit récupérer de nouveaux la liste des providers en cas d'erreur
+			model.addAttribute("providers", providerService.listProvider());
+			return "article/addArticle";
 		}
-    	
-		 article.setPicture(fileName.toString());
 
-    	// fin upload
-    	 articleService.saveArticle(article);
-    	 return "redirect:list";
-    	
-    	//return article.getLabel() + " " +article.getPrice() + " " + p.toString();
-    }
-    
-    @GetMapping("delete/{id}")
-    public String deleteArticle(@PathVariable("id") long id) {
-        articleService.findArticleById(id)
-            .orElseThrow(()-> new IllegalArgumentException("Invalid article Id:" + id));
-        articleService.deleteArticle(id);
-        return "redirect:../list";
-    }
-
-    @GetMapping("edit/{id}")
-    public String showArticleFormToUpdate(@PathVariable("id") long id, Model model) {
-    	try {
-    	Article article = articleService.findArticleById(id)
-            .orElseThrow(()->new IllegalArgumentException("Invalid article Id:" + id));
-    	 model.addAttribute("article", article);
-         model.addAttribute("providers", providerService.listProvider());
-         //model.addAttribute("idProvider", article.getProvider().getId());
-    	}
-    	catch(IllegalArgumentException ex)
-    	{
-    		return "article/500.html";
-    	}
-       
-        
-        return "article/updateArticle";
-    }
-
-    @PostMapping("edit")
-    public String updateArticle(@Valid Article article, BindingResult result,
-        Model model, @RequestParam(name = "providerId", required = false) Long p,
-        @RequestParam("files") MultipartFile[] files) {
-        if (result.hasErrors()) {
-        	
-            return "article/updateArticle";
-        }
-        
-        Provider provider = providerService.findProviderById(p)
-                .orElseThrow(()-> new IllegalArgumentException("Invalid provider Id:" + p));
-    	article.setProvider(provider);
-    	
-    	
-    	// upload de l'image
-    	
-    	MultipartFile file = files[0];
-    	if(file.getOriginalFilename()!=null) {
-    	long id = new Date().getTime();
-    	Path fileNameAndPath = Paths.get(uploadDirectory, ""+id+file.getOriginalFilename());
-    	
-    	StringBuilder fileName = new StringBuilder();
-    	
-    	fileName.append(""+id+file.getOriginalFilename()); //nom du ficher
-		  try {
-			Files.write(fileNameAndPath, file.getBytes()); //ecriture des bytes du fichier dans le nouveau emplacement
 		
-			//delete de l'ancienne image
+		// fin upload
+		articleService.saveArticle(article,p,files);
+		return "redirect:list";
 
-		    	String urlImage = article.getPicture();
-		    	if(urlImage!=null) {
-		    	Path path = Paths.get(ArticleController.uploadDirectory,urlImage);
-		        
-		    	try {
-					Files.deleteIfExists(path);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		    	}
-			
-			//fin delete picture
-		  
-		  } catch (IOException e) {
-			e.printStackTrace();
+		// return article.getLabel() + " " +article.getPrice() + " " + p.toString();
+	}
+
+	@GetMapping("delete/{id}")
+	public String deleteArticle(@PathVariable("id") long id) {
+		articleService.findArticleById(id).orElseThrow(() -> new IllegalArgumentException("Invalid article Id:" + id));
+		articleService.deleteArticle(id);
+		return "redirect:../list";
+	}
+
+	@GetMapping("edit/{id}")
+	public String showArticleFormToUpdate(@PathVariable("id") long id, Model model) {
+		try {
+			Article article = articleService.findArticleById(id)
+					.orElseThrow(() -> new IllegalArgumentException("Invalid article Id:" + id));
+			model.addAttribute("article", article);
+			model.addAttribute("providers", providerService.listProvider());
+			// model.addAttribute("idProvider", article.getProvider().getId());
+		} catch (IllegalArgumentException ex) {
+			return "article/500.html";
 		}
-    	
-		 article.setPicture(fileName.toString());
-    	}
-    	//
-    	
-        articleService.saveArticle(article);
-        
-        return "redirect:list";
-    }
-    
-    @GetMapping("show/{id}")
-    public String showArticleDetails(@PathVariable("id") long id, Model model) {
-    	Article article = articleService.findArticleById(id)
-            .orElseThrow(()->new IllegalArgumentException("Invalid provider Id:" + id));
-    	
-        model.addAttribute("article", article);
-        
-        return "article/showArticle";
-    }
 
+		return "article/updateArticle";
+	}
 
+	@PostMapping("edit")
+	public String updateArticle(@Valid Article article, BindingResult result, Model model,
+			@RequestParam(name = "providerId", required = false) Long p, @RequestParam("files") MultipartFile[] files) {
+		if (result.hasErrors()) {
 
-    
+			return "article/updateArticle";
+		}
+
+		
+		articleService.updateArticle(article,p,files);
+
+		return "redirect:list";
+	}
+
+	@GetMapping("show/{id}")
+	public String showArticleDetails(@PathVariable("id") long id, Model model) {
+		Article article = articleService.findArticleById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + id));
+
+		model.addAttribute("article", article);
+
+		return "article/showArticle";
+	}
+
 }
--- src/main/java/com/sip/ams/services/ArticleService.java
+++ src/main/java/com/sip/ams/services/ArticleService.java
@@ -4,15 +4,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sip.ams.controllers.ArticleController;
 import com.sip.ams.entities.Article;
+import com.sip.ams.entities.Provider;
 import com.sip.ams.repositories.ArticleRepository;
+import com.sip.ams.repositories.ProviderRepository;
 
 @Service
 public class ArticleService {
@@ -20,9 +24,31 @@ public class ArticleService {
 	@Autowired  // Spring va se charger de créer un objet qui implémente cette interface
 	ArticleRepository articleRepository ;
 	
+	@Autowired  // Spring va se charger de créer un objet qui implémente cette interface
+	ProviderRepository providerRepository ;
 	
-	public Article saveArticle(Article article)
+	public Article saveArticle(Article article, long p, MultipartFile[] files)
 	{
+		Provider provider = providerRepository.findById(p)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + p));
+		article.setProvider(provider);
+		// upload de l'image
+
+		MultipartFile file = files[0];
+		long id = new Date().getTime();
+		Path fileNameAndPath = Paths.get(ArticleController.uploadDirectory, "" + id + file.getOriginalFilename());
+
+		StringBuilder fileName = new StringBuilder();
+
+		fileName.append("" + id + file.getOriginalFilename()); // nom du ficher
+		try {
+			Files.write(fileNameAndPath, file.getBytes()); // ecriture des bytes du fichier dans le nouveau emplacement
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		article.setPicture(fileName.toString());
+
 		return articleRepository.save(article);  // sauvegarder dans la base
 		
 	}
@@ -58,6 +84,55 @@ public class ArticleService {
 	public Optional<Article> findArticleById(long id)
 	{
 		return articleRepository.findById(id);
+	}
+	
+	public Article updateArticle(Article article, long p, MultipartFile[] files)
+	{
+		Provider provider = providerRepository.findById(p)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + p));
+		article.setProvider(provider);
+
+		// upload de l'image
+
+		MultipartFile file = files[0];
+		if (!file.isEmpty()) {
+			// System.out.println("Article name : " +article.getPicture());
+			// System.out.println("File name : " +file.getOriginalFilename());
+			long id = new Date().getTime();
+			Path fileNameAndPath = Paths.get(ArticleController.uploadDirectory, "" + id + file.getOriginalFilename());
+
+			StringBuilder fileName = new StringBuilder();
+
+			fileName.append("" + id + file.getOriginalFilename()); // nom du ficher
+			try {
+				Files.write(fileNameAndPath, file.getBytes()); // ecriture des bytes du fichier dans le nouveau
+																// emplacement
+
+				// delete de l'ancienne image
+
+				String urlImage = article.getPicture();
+				if (urlImage != null) {
+					Path path = Paths.get(ArticleController.uploadDirectory, urlImage);
+
+					try {
+						Files.deleteIfExists(path);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+
+				// fin delete picture
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			article.setPicture(fileName.toString());
+		}
+		//
+		// article.setPicture(fileName.toString());
+		return articleRepository.save(article);
 	}
 	
 }
