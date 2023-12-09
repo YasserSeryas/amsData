df773e70b04110576d73086a06122dee78527e11
moahmed amine mezghich <git@mezghich.com>
2023-12-09 13:27:49 +0100

    partie 2 security : ajout des pages html

--- src/main/java/com/sip/ams/entities/User.java
+++ src/main/java/com/sip/ams/entities/User.java
@@ -14,6 +14,11 @@ import java.util.Set;
 @Entity
 @Table(name = "user")
 public class User implements UserDetails{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "user_id")
